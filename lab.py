756c7769f461af869e2f83c9637146af5db91ee5
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2019-03-07 11:21:04 +0100

    refactor, init curvefit

--- lab.py
+++ lab.py
@@ -1,5 +1,9 @@
 import os
+import numpy as np
+from scipy.optimize import curve_fit
+
 import iptrack as ip
+import truevalues as tv
 
 
 data_folder = "data/"
@@ -14,8 +18,42 @@ def get_iptrack():
 
     return data
 
-if __name__ == "__main__":
+
+def curvefit():
     for filename, data in get_iptrack().items():
         print(filename)
+        data = data[0]
+        print(data)
+        x_max, y_max = data[0][1], data[0][2]
+        max_cor = np.array([(x_max, y_max)])
+        np.append(max_cor, [(x_max, y_max)], axis=0)
+        print(max_cor)
+
+
+        xlast, ylast = x_max, y_max
+        xnext, ynext = 0, 0
+        for i in range(len(data) -3):
+           if data[i][2] < ylast:
+               np.append(max_cor, [(data[i][1], data[i][2])], axis=0)
+
+#b = curvefit
+
+def euler():
+    for filename, data in get_iptrack().items():
+        print("\n" + filename)
         x_start = data[0][0][1]
 
+        iptrack = data[1]
+
+        print("x: ", x_start)
+        print("iptrack: ", iptrack)
+
+        tvs = tv.trvalues(iptrack, x_start)
+
+        print("tv: ", tvs)
+
+
+if __name__ == "__main__":
+    print("whoop \n\n")
+
+    curvefit()
