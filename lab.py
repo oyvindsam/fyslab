3ad32e831a509a60ff1c450c8599af7236eb176d
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2019-03-14 11:14:55 +0100

    Implement curvefit

--- lab.py
+++ lab.py
@@ -10,7 +10,7 @@ data_folder = "data/"
 out_folder = "out/"
 
 
-# Returns list<np.array>
+# Returns dict{'filename': (data: np.array, polyfit: np.array)}
 def get_iptrack():
     data = {}  # 'filename': string -> (tracker_data, iptrack_data): tuple
     for filename in os.listdir(os.getcwd() + "/" + data_folder):
@@ -18,10 +18,11 @@ def get_iptrack():
 
     return data
 
-
-def curvefit(coordinates: np.array):
-
+# input: tracker data for one file
+# returns max_cor: np.array [[xcor ycor]]
+def extract_maxvalues(coordinates: np.array):
     data = coordinates
+
     # compare values before and after current value to check if current is the topmost.
     def checkrange(data, n, i):
         for k in range(i - n, i + n):
@@ -32,19 +33,30 @@ def curvefit(coordinates: np.array):
     # get the index of the point with the highest y-value in the first 15 elements
     max_index = np.argmax(np.max(data[:15, [2]], axis=1))
     x_max, y_max = data[max_index][1], data[max_index][2]
-    max_cor = np.array([(x_max, y_max)])
+    max_cor = np.array([(x_max, y_max)])  # initialize array
 
-    # we already have the highest coordinates, so just start at 20.
-    for i in range(20, len(data) -5):
+    # we already have the highest coordinates, so just start at index 20.
+    for i in range(20, len(data)-1):
         if checkrange(data, 5, i):
             max_cor = np.append(max_cor, [(data[i][1], data[i][2])], axis=0)
-
     return max_cor
 
 
-#b = curvefit
+def curvefit(max_cor: np.array):
 
-def euler():
+    # used in scipy.optimize.curve_fit()
+    def curvefit_func(x_max: np.array, a, b):
+        return a * np.exp(-b * x_max)
+
+    # covert x- and y-values to 1 dim arrays
+    xdata, ydata = max_cor[:,[0]].flatten(), max_cor[:,[1]].flatten()
+
+    # use scipy function. values in fit corresponds to 'a' and 'b' in curvefit_func()
+    fit, covar = curve_fit(curvefit_func, xdata, ydata)
+    return fit, covar
+
+
+def euler(max_values):
     for filename, data in get_iptrack().items():
         print("\n" + filename)
         x_start = data[0][0][1]
@@ -63,10 +75,16 @@ if __name__ == "__main__":
     print("whoop \n\n")
 
     iptrack_data = get_iptrack()
-    n = iptrack_data.keys()
+    filenames = iptrack_data.keys()
 
-
-    for filename, data in iptrack_data.items():
+    for filename in filenames:
         print(filename)
-        print(curvefit(data[0]))
+        tracker_data = iptrack_data[filename][0]
+        maxvalues = extract_maxvalues(tracker_data)
+
+        fit, covar = curvefit(maxvalues)
+        a, b = fit[0], fit[1]
+
+        print("\n\nA: %s\nb: %s " % (fit[0], fit[1]))
+        print("covar: ", covar)
 
