7c75365326fbd61e7a24289a83a4191e074e4cd5
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2019-03-14 12:23:51 +0100

    Init euler

--- lab.py
+++ lab.py
@@ -10,8 +10,8 @@ data_folder = "data/"
 out_folder = "out/"
 
 
-# Returns dict{'filename': (data: np.array, polyfit: np.array)}
-def get_iptrack():
+# Returns dict{'filename': (tracker_data: np.array, iptrack: np.array)}
+def get_data():
     data = {}  # 'filename': string -> (tracker_data, iptrack_data): tuple
     for filename in os.listdir(os.getcwd() + "/" + data_folder):
         data[filename] = ip.iptrack(data_folder + filename)
@@ -41,7 +41,6 @@ def extract_maxvalues(coordinates: np.array):
             max_cor = np.append(max_cor, [(data[i][1], data[i][2])], axis=0)
     return max_cor
 
-
 def curvefit(max_cor: np.array):
 
     # used in scipy.optimize.curve_fit()
@@ -55,36 +54,61 @@ def curvefit(max_cor: np.array):
     fit, covar = curve_fit(curvefit_func, xdata, ydata)
     return fit, covar
 
+def acc(alpha, c, v, m):
+    g = 9.8214675
+    acc = (5/7) * g * np.sin(alpha) - (c * v / m)
+    return acc
 
-def euler(max_values):
-    for filename, data in get_iptrack().items():
-        print("\n" + filename)
-        x_start = data[0][0][1]
+def euler(coordinates, iptrack, h):
+    xn = coordinates[0][1]
 
-        iptrack = data[1]
+    approx = np.array([(xn, 0.)])
 
-        print("x: ", x_start)
-        print("iptrack: ", iptrack)
+    for i in range(10):
+        tvs = tv.trvalues(iptrack, xn)
+        y, dydx, d2ydx2, alpha, R = tvs
 
-        tvs = tv.trvalues(iptrack, x_start)
 
-        print("tv: ", tvs)
+        # TODO: whaaat ----------------------------------
+        vn1 = dydx + h*d2ydx2
+        xn = xn + abs(vn1) * np.cos(alpha)
+        yn = y + h*dydx
+        # -----------------------------------------------
 
+        print("\ntvs: ", tvs)
+        print("\nvn1: %s\nxn: %s\nyn: %s" % (vn1, xn, yn))
+
+        approx = np.append(approx, [(xn, yn)], axis=0)
 
 if __name__ == "__main__":
     print("whoop \n\n")
 
-    iptrack_data = get_iptrack()
-    filenames = iptrack_data.keys()
+    CURVEFIT = False
+    EULER = True
 
-    for filename in filenames:
+    data = get_data()
+    filenames = data.keys()
+
+    if CURVEFIT:
+        for filename in filenames:
+            print(filename)
+            tracker_data = data[filename][0]
+            maxvalues = extract_maxvalues(tracker_data)
+
+            fit, covar = curvefit(maxvalues)
+            #std = np.sqrt(np.diag(covar))
+            a, b = fit[0], fit[1]
+
+            print("\n\nA: %s\nb: %s " % (fit[0], fit[1]))
+            print("covar: ", covar)
+
+    if EULER:
+        filename = '51.txt'
+        #for filename in filenames:
         print(filename)
-        tracker_data = iptrack_data[filename][0]
+        tracker_data = data[filename][0]
+        iptrack = data[filename][1]
         maxvalues = extract_maxvalues(tracker_data)
+        approx = euler(maxvalues, iptrack, 0.005)
 
-        fit, covar = curvefit(maxvalues)
-        a, b = fit[0], fit[1]
-
-        print("\n\nA: %s\nb: %s " % (fit[0], fit[1]))
-        print("covar: ", covar)
-
+        print(approx)
